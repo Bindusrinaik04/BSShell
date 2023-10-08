c7af2c8d7c3384c633b6107ed4cc839cdad15f60
SUGALI RAMAVATH BINDU SRI <87903425+Bindusrinaik04@users.noreply.github.com>
2023-10-08 11:16:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -17,8 +17,10 @@ void getstmt(){
   char usrCommand[MAX_STATEMENT_LENGH]; //primary user command
   char remusrCommand[MAX_STATEMENT_LENGTH]; //if there are any options ex:ls -l -a ,ls is primary command,-l -a are stored in remusrCommand 
   int p=getusrCommand(usrCommand,remusrCommand,MAX_STATEMENT_LENGTH);//p is used here to indicate whether pipe is present or not
-  if(strlen(usrCommand)==0) return ; //if usrCommand length = 0 return meaning the usr did not give any primary input command
-  
+  if(strlen(usrCommand)==0) return ; //if usrCommand length = 0 return meaning the usr did not give any primary input command so return
+  char* argv[MAX_STATEMENT_LENGTH];
+  int count=0;
+  tokenize(usrCommand,argv,MAX_STATEMENT_LENGTH,count);
   
 }
 
