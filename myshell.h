230f52fa2dcff7177c0a9e49cb4dec8f65113345
SUGALI RAMAVATH BINDU SRI <87903425+Bindusrinaik04@users.noreply.github.com>
2023-10-08 19:17:28 +0530

    Update myshell.h

--- myshell.h
+++ myshell.h
@@ -1,14 +1,13 @@
-#ifndef BSShell
+ #ifndef BSShell
 #define BSShell
-
-//maximum sizes for host name,username,current working directory,shell command statement
 #define MAX_HOSTNAME_SIZE 256
 #define MAX_USERNAME_SIZE 256
 #define MAX_CWDPATH_SIZE 1024
 #define MAX_STATEMENT_LENGTH 2048
 
+
 //functions prototype
-void initPath();
+void initpath();
 
 void getprompt(char *);
 void printError(char *);
@@ -19,5 +18,6 @@ int tokenize(char *, char **, int, int *);
 char *trim(char *);
 char *rStr(char *);
 int findExecutable(char *,char *);
-#endif
 
+#endif
+//maximum sizes for host name,username,current working directory,shell command statement
